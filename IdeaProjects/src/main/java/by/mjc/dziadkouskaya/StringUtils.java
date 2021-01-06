a8e1f68ae1ba6dda35555f636281ac774fc28cbd
Yana Dziadkouskaya <yana.dziadkouskaya@gmail.com>
2021-01-06 12:56:17 +0300

    Gradle Task 1

---
+++ IdeaProjects/src/main/java/by/mjc/dziadkouskaya/StringUtils.java
@@ -0,0 +1,13 @@
+package by.mjc.dziadkouskaya;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
+public class StringUtils {
+
+    public static boolean isPositiveNumber(String str) {
+        return isNumeric(str);
+
+    }
+
+
+}
